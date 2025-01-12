cf3633e2b721b438836e06e7f815eb6875511c09
korrenhannes <134727678+korrenhannes@users.noreply.github.com>
2025-01-12 18:38:07 +0200

    a

--- mulmat.asm
+++ mulmat.asm
@@ -42,54 +42,98 @@ L_k:
   # M1[i,k]: address = s0 + (i*4 + k)
   # offset_i_k = i*4 + k
   #---------------------------------------------------------
-  sll  $v0, $t0, $imm1, $zero, 2, 0   # v0 = i * 4
-  add  $v0, $v0, $t2,   $zero, 0, 0   # v0 = i*4 + k
-  add  $a1, $s0, $v0,   $zero, 0, 0   # a1 = base(M1) + offset
-  lw   $v0, $a1, $zero, $zero, 0, 0   # v0 = M1[i,k]
+  sll  $v0, $t0, $imm1, $zero, 2, 0    # v0 = i * 4
+  add  $v0, $v0, $t2,   $zero, 0, 0    # v0 = i*4 + k
+  add  $a1, $s0, $v0,   $zero, 0, 0    # a1 = base(M1) + offset
+  lw   $v0, $a1, $zero, $zero, 0, 0    # v0 = M1[i,k]
 
   #---------------------------------------------------------
   # M2[k,j]: address = s1 + (k*4 + j)
   # offset_k_j = k*4 + j
   #---------------------------------------------------------
-  sll  $a1, $t2, $imm1, $zero, 2, 0   # a1 = k*4
-  add  $a1, $a1, $t1,   $zero, 0, 0   # a1 = k*4 + j
-  add  $a1, $s1, $a1,   $zero, 0, 0   # a1 = base(M2) + offset
-  lw   $a1, $a1, $zero, $zero, 0, 0   # a1 = M2[k,j]
+  sll  $a1, $t2, $imm1, $zero, 2, 0    # a1 = k*4
+  add  $a1, $a1, $t1,   $zero, 0, 0    # a1 = k*4 + j
+  add  $a1, $s1, $a1,   $zero, 0, 0    # a1 = base(M2) + offset
+  lw   $a1, $a1, $zero, $zero, 0, 0    # a1 = M2[k,j]
 
   # sum += M1[i,k] * M2[k,j]
-  mac  $a0, $v0, $a1,   $a0,   0, 0
+  mac  $a0, $v0, $a1,   $a0,   0, 0    # a0 = a0 + (v0 * a1)
 
   # k++
-  add  $t2, $t2, $imm1, $zero, 1, 0
-  sub  $v0, $t2, $imm1, $zero, 4, 0
-  # We want: if (t2 < 4) => jump to L_k.
-  # That means if (v0 < 0) => L_k.
-  # => "blt $v0, $zero, $imm1, L_k, 0, 0" would treat L_k as a register (error).
-  # FIX -> put L_k in an immediate field:
-  blt  $v0, $zero, $zero, $imm1, L_k, 0
+  add  $t2, $t2, $imm1, $zero, 1, 0    # t2++
+  sub  $v0, $t2, $imm1, $zero, 4, 0    # v0 = t2 - 4
+
+  # if (t2 < 4) => (v0 < 0) => jump to L_k
+  # IMPORTANT FIX: rs=$v0, rt=$zero => "if (v0 < 0)"
+  blt  $zero, $v0, $zero, $imm1, L_k, 0
 
   #---------------------------------------------------------
   # Store sum in M3[i,j]: M3[i,j] = sum
   # offset_i_j = i*4 + j
   #---------------------------------------------------------
-  sll  $v0, $t0, $imm1, $zero, 2, 0   # v0 = i*4
-  add  $v0, $v0, $t1,   $zero, 0, 0   # v0 = i*4 + j
-  add  $a1, $s2, $v0,   $zero, 0, 0   # a1 = base(M3) + offset
-  sw   $a1, $zero, $a0, $zero, 0, 0   # M3[i,j] = sum
+  sll  $v0, $t0, $imm1, $zero, 2, 0    # v0 = i*4
+  add  $v0, $v0, $t1,   $zero, 0, 0    # v0 = i*4 + j
+  add  $a1, $s2, $v0,   $zero, 0, 0    # a1 = base(M3) + offset
+
+  # IMPORTANT FIX: store $a0 into MEM[a1]
+  # sw format: MEM[R[rs]+R[rt]] = R[rm] + R[rd]
+  # We want address = $a1, data = $a0 => so rs=$a1, rt=$zero, rm=$a0, rd=$zero
+  sw   $zero, $a1, $zero, $a0, 0, 0    # M3[i,j] = sum
 
   # j++
   add  $t1, $t1, $imm1, $zero, 1, 0
-  sub  $v0, $t1, $imm1, $zero, 4, 0
-  # if (t1 < 4) => jump to L_j
-  # => if (v0 < 0) => L_j
-  blt  $v0, $zero, $zero, $imm1, L_j, 0
+  sub  $v0, $t1, $imm1, $zero, 4, 0    # v0 = t1 - 4
+
+  # if (t1 < 4) => (v0 < 0) => jump to L_j
+  # Again, rs=$v0, rt=$zero => "if (v0 < 0)"
+  blt  $zero, $v0, $zero, $imm1, L_j, 0
 
 # i++
 add  $t0, $t0, $imm1, $zero, 1, 0
-sub  $v0, $t0, $imm1, $zero, 4, 0
-# if (t0 < 4) => jump to L_i
-# => if (v0 < 0) => L_i
-blt  $v0, $zero, $zero, $imm1, L_i, 0
+sub  $v0, $t0, $imm1, $zero, 4, 0    # v0 = t0 - 4
+
+# if (t0 < 4) => (v0 < 0) => jump to L_i
+blt  $zero, $v0, $zero, $imm1, L_i, 0
 
 # Done
 halt $zero, $zero, $zero, $zero, 0, 0
+
+############################################################
+# Initialize data memory with two 4x4 matrices (M1 & M2).
+# M1: addresses 0x100 to 0x10F (256 to 271)
+# M2: addresses 0x110 to 0x11F (272 to 287)
+############################################################
+
+    .word 256 1
+    .word 257 2 
+    .word 258 3
+    .word 259 4
+    .word 260 5
+    .word 261 6 
+    .word 262 7
+    .word 263 8
+    .word 264 9
+    .word 265 10
+    .word 266 11
+    .word 267 12
+    .word 268 13
+    .word 269 14
+    .word 270 15
+    .word 271 16
+
+    .word 272 1
+    .word 273 2 
+    .word 274 3
+    .word 275 4
+    .word 276 5
+    .word 277 6 
+    .word 278 7
+    .word 279 8
+    .word 280 9
+    .word 281 10
+    .word 282 11
+    .word 283 12
+    .word 284 13
+    .word 285 14
+    .word 286 15
+    .word 287 16
