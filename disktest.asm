80e0dad8b9683d79e828b25fb286b60b80e5aa55
korrenhannes <134727678+korrenhannes@users.noreply.github.com>
2025-01-22 14:26:35 +0200

    a

--- disktest.asm
+++ disktest.asm
@@ -1,42 +1,115 @@
-#in rd=IO[rs+rt]
-#out IO[rs+rt]=rm
-#rd rs rt rm
-	out $zero, $imm1, $zero, $imm1, 1, 0 #enable irq1
-	out $zero, $imm1, $zero, $imm2, 6, L3		# set irqhandler as L3
-	in $t1, $imm1, $zero, $zero, 17, 0 #t1=diskstatus
-	out $zero, $imm1, $zero, $imm2, 16, 0 #set buffer
-	add $s0, $imm1, $zero, $zero, 7, 0
-	beq $zero, $t1, $imm1, $imm2, 0, L1 #jump if diskstatus = 0
-	halt $zero, $zero, $zero, $zero, 0, 0
-MAIN:
-	sub $s0, $s0, $imm1, $zero, 2, 0
-	bge $zero, $s0, $zero, $imm1, L1, 0
-	blt $zero, $s0, $zero, $imm1, ENDMAIN, 0
-L1:
-	out $zero, $imm1, $zero, $s0, 15, 0 #set sector number
-	out $zero, $imm1, $zero, $imm2, 14, 1 #set read command
-	add $t0, $zero, $zero, $zero, 0, 0
-	blt $zero, $t0, $imm1, $imm2, 1024, LOOPREAD
-L2:
-	add $s0, $s0, $imm1, $zero, 1, 0
-	out $zero, $imm1, $zero, $s0, 15, 0 #set sector number
-	out $zero, $imm1, $zero, $imm2, 14, 2 #set write command
-	add $t0, $zero, $zero, $zero, 0, 0
-	blt $zero, $t0, $imm1, $imm2, 1024, LOOPWRITE
+######################################################
+# "in"  => rd = IO[rs + rt]
+# "out" => IO[rs + rt] = rm
+#
+# Using: rd rs rt rm
+######################################################
+
+# 1) Enable IRQ1
+out   $zero,  $imm1,  $zero, $imm1,  1, 0       # IO[1] = 1
+
+# 2) Set IRQHANDLER to label IRQRoutine
+out   $zero,  $imm1,  $zero, $imm2,  6,  IRQRoutine
+
+# 3) $t1 = DISKSTATUS (IO[17])
+in    $t1,    $imm1,  $zero, $zero,  17, 0
+
+# 4) Set DISKBUFFER (IO[16]) = 0
+out   $zero,  $imm1,  $zero, $imm2,  16, 0
+
+# 5) Initialize $s0 = 7
+add   $s0,    $imm1,  $zero, $zero,  7,  0
+
+# If diskstatus == 0 => jump to PROCESS
+beq   $zero,  $t1,    $imm1, $imm2,  0,  PROCESS
+
+# Otherwise => halt
+halt  $zero,  $zero,  $zero, $zero,  0,  0
+
+
+######################################################
+# Main loop
+######################################################
+PROCESS:
+    sub   $s0,   $s0,   $imm1,  $zero, 2, 0     # s0 -= 2
+
+    # If s0 >= 0 => jump to SECTOR_READ
+    bge   $zero, $s0,   $zero,  $imm1, SECTOR_READ, 0
+
+    # Else if s0 < 0 => jump to FINISHED
+    blt   $zero, $s0,   $zero,  $imm1, FINISHED, 0
+
+######################################################
+# 1) SECTOR_READ
+######################################################
+SECTOR_READ:
+    # out IO[15] = s0   (sector number)
+    out   $zero, $imm1,  $zero, $s0,   15,  0
+    
+    # out IO[14] = 1    (read command)
+    out   $zero, $imm1,  $zero, $imm2, 14,  1
+
+    # t0 = 0
+    add   $t0,   $zero,  $zero, $zero, 0,   0
+
+    # Wait 1024 cycles => check t0 < 1024
+    blt   $zero, $t0,    $imm1, $imm2, 1024, LOOPREAD
+
+######################################################
+# 2) SECTOR_WRITE
+######################################################
+WRITE_STEP:
+    # s0++
+    add   $s0,   $s0,    $imm1, $zero, 1,   0
+
+    # out IO[15] = s0   (sector #)
+    out   $zero, $imm1,  $zero, $s0,   15,  0
+
+    # out IO[14] = 2    (write command)
+    out   $zero, $imm1,  $zero, $imm2, 14,  2
+
+    # t0 = 0
+    add   $t0,   $zero,  $zero, $zero, 0,   0
+
+    # Wait 1024 cycles => check t0 < 1024
+    blt   $zero, $t0,    $imm1, $imm2, 1024, LOOPWRITE
+
+
+######################################################
+# Reading loop
+######################################################
 LOOPREAD:
-	add $t0, $t0, $imm1, $zero, 1, 0
-	blt $zero, $t0, $imm1, $imm2, 1024, LOOPREAD
-	beq $zero, $t0, $imm1, $imm2, 1024, ENDREAD
+    add   $t0,   $t0,    $imm1, $zero, 1,   0
+    blt   $zero, $t0,    $imm1, $imm2, 1024, LOOPREAD
+    beq   $zero, $t0,    $imm1, $imm2, 1024, ENDREAD
+
 ENDREAD:
-	beq $zero, $zero, $zero, $imm1, L2, 0
+    # After read done => jump to WRITE_STEP
+    beq   $zero, $zero,  $zero, $imm1, WRITE_STEP, 0
+
+
+######################################################
+# Writing loop
+######################################################
 LOOPWRITE:
-	add $t0, $t0, $imm1, $zero, 1, 0
-	blt $zero, $t0, $imm1, $imm2, 1024, LOOPWRITE
-	beq $zero, $t0, $imm1, $imm2, 1024, ENDWRITE
+    add   $t0,   $t0,    $imm1, $zero, 1,   0
+    blt   $zero, $t0,    $imm1, $imm2, 1024, LOOPWRITE
+    beq   $zero, $t0,    $imm1, $imm2, 1024, ENDWRITE
+
 ENDWRITE:
-	beq $zero, $zero, $zero, $imm1, MAIN, 0
-ENDMAIN:
-	halt $zero, $zero, $zero, $zero, 0, 0
-L3:
-	add $t2, $t2, $imm1, $zero, 1, 0
-	reti $zero, $zero, $zero, $zero, 0, 0		# return from interrupt
+    # After write => go back to PROCESS
+    beq   $zero, $zero,  $zero, $imm1, PROCESS, 0
+
+FINISHED:
+    halt  $zero, $zero,  $zero,  $zero, 0,  0
+
+
+######################################################
+# IRQ Service Routine
+######################################################
+IRQRoutine:
+    # $t2++
+    add   $t2,   $t2,    $imm1,  $zero, 1, 0
+
+    # Return from interrupt
+    reti  $zero, $zero,  $zero,  $zero, 0,  0
